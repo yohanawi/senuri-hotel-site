b7e1dbd761e58d74bfa6d8439e6811f6f382f0a7
yohanawi <yohan.indrawansha2000@gmail.com>
2025-05-16 11:15:44 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/images/favicon.ico?v=1" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         {children}
       </body>
